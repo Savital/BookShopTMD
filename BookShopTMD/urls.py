a814d36323484aee0c1a31a1988730a7e982e760
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-03-30 15:46:43 +0300

    Edited settings

--- BookShopTMD/urls.py
+++ BookShopTMD/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# urls paths
 urlpatterns = [
     url(r'^BookShop/', include('BookShop.urls')),
     url(r'^', include('BookShop.urls', namespace='BookShop')),
