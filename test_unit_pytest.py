cb2a53c226c3203aad3e76713c28db6fd7813d8f
savital <ssr.vitamin@gmail.com>
2019-04-10 23:36:57 +0300

    Added unit_pytest module

---
+++ test_unit_pytest.py
@@ -0,0 +1,16 @@
+class hello:
+    message = "test_message"
+
+    def __init__(self):
+        pass
+
+    def set(self, message):
+        self.message = message
+
+    def show(self):
+        print(self.message)
+
+def test_hello_world():
+    h = hello()
+    h.set("test")
+    assert h.message == "test"
