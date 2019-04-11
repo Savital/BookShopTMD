4a0ea6686ccaf303d64e7d2177e13af852c7a129
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-11 12:09:34 +0300

    Added Customer class.

--- BookShop/models.py
+++ BookShop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    customer_id = models.Index
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "Customer %s %s" % (self.first_name, self.last_name)
+
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customers'
