2869f4f6e240ce2fde5d7e5737f6a9e7581038ad
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-11 15:51:02 +0300

    Added urlpatterns.

--- BookShopTMD/urls.py
+++ BookShopTMD/urls.py
@@ -1,5 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.autodiscover()
 
 # urls paths
 urlpatterns = [
@@ -7,3 +11,6 @@ urlpatterns = [
     url(r'^', include('BookShop.urls', namespace='BookShop')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
