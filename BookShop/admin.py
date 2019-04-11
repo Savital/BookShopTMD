70c5c1b1dad53b88577f23d496741b0ec02d8510
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-11 15:52:27 +0300

    Added Order Class.

--- BookShop/admin.py
+++ BookShop/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
 class CustomerAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Customer._meta.fields]
     search_fields = [field.name for field in Customer._meta.fields]
@@ -11,3 +10,15 @@ class CustomerAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Customer
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.fields]
+    search_fields = [field.name for field in Order._meta.fields]
+    list_filter = [field.name for field in Order._meta.fields]
+    exclude = []
+
+    class Meta:
+        model = Order
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
--- BookShop/models.py
+++ BookShop/models.py
@@ -12,3 +12,16 @@ class Customer(models.Model):
     class Meta:
         verbose_name = 'Customer'
         verbose_name_plural = 'Customers'
+
+class Order(models.Model):
+    order_id = models.Index
+    #customer_id = models.ForeignKey(Customer)
+    date = models.DateField(auto_now_add=True, auto_now=False)
+    time = models.TimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return "Order %s" % self.id
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
