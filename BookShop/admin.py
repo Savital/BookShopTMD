ecd85a5316843218d6c8b1775bcb4134e0852cda
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-12 19:13:27 +0300

    Fixed bug with 500 response.

--- BookShop/admin.py
+++ BookShop/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Customer._meta.fields]
     search_fields = [field.name for field in Customer._meta.fields]
@@ -11,14 +10,4 @@ class CustomerAdmin(admin.ModelAdmin):
     class Meta:
         model = Customer
 
-class OrderAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Order._meta.fields]
-    search_fields = [field.name for field in Order._meta.fields]
-    list_filter = [field.name for field in Order._meta.fields]
-    exclude = []
-
-    class Meta:
-        model = Order
-
 admin.site.register(Customer, CustomerAdmin)
-admin.site.register(Order, OrderAdmin)
--- BookShop/models.py
+++ BookShop/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
     customer_id = models.Index
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
 
     def __str__(self):
         return "Customer %s %s" % (self.first_name, self.last_name)
@@ -12,16 +12,3 @@ class Customer(models.Model):
     class Meta:
         verbose_name = 'Customer'
         verbose_name_plural = 'Customers'
-
-class Order(models.Model):
-    order_id = models.Index
-    #customer_id = models.ForeignKey(Customer)
-    date = models.DateField(auto_now_add=True, auto_now=False)
-    time = models.TimeField(auto_now_add=True, auto_now=False)
-
-    def __str__(self):
-        return "Order %s" % self.id
-
-    class Meta:
-        verbose_name = 'Order'
-        verbose_name_plural = 'Orders'
