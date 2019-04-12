896ac1cb807f377ab994bf4517cf1c6f819cfb2e
Oddush <sashapak97@mail.ru>
2019-04-12 21:50:13 +0300

    Update admin.py

--- BookShop/admin.py
+++ BookShop/admin.py
@@ -27,7 +27,37 @@ class ProducersAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Producers
+      
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+    search_fields = [field.name for field in Product._meta.fields]
+    list_filter = [field.name for field in Product._meta.fields]
+    exclude = []
+
+    class Meta:
+        model = Product
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Shop._meta.fields]
+    search_fields = [field.name for field in Shop._meta.fields]
+    list_filter = [field.name for field in Shop._meta.fields]
+    exclude = []
+
+    class Meta:
+        model = Shop
+
+class RelationsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Relations._meta.fields]
+    search_fields = [field.name for field in Relations._meta.fields]
+    list_filter = [field.name for field in Relations._meta.fields]
+    exclude = []
+
+    class Meta:
+        model = Relations    
 
 admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Producers, ProducersAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Shop, ShopAdmin)
+admin.site.register(Relations, RelationsAdmin)
