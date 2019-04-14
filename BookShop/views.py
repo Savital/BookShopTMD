58ec80f033db59158ceaa3e645ac3b0e6a527092
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-14 12:02:02 +0300

    Added views.py

--- BookShop/views.py
+++ BookShop/views.py
@@ -1,5 +1,42 @@
+from django.db.models import *
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic.edit import FormView
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
+from django.views.generic.base import View, logger
 
-def index(request):
-    return HttpResponse("BookShop MainPage")
+from BookShop.models import *
+
+def contact(request):
+    return render(request, './contact.html', locals())
+
+def product(request):
+    assert isinstance(request, HttpRequest)
+    return render(request, './contact.html', locals())
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+    success_url = "/login/"
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "login.html"
+    success_url = "/"
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect("/")
+
+
