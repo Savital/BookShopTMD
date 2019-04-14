ce86b7a027f8fa080a12a7ec512be96932f97f6e
Oddush <sashapak97@mail.ru>
2019-04-14 13:26:30 +0300

    Update urls.py

--- BookShop/urls.py
+++ BookShop/urls.py
@@ -4,12 +4,12 @@ from BookShop import views
 
 app_name = 'BookShop'
 urlpatterns = [
-    url(r'^register/$', views.RegisterFormView.as_view()),
-    url(r'^login/$', views.LoginFormView.as_view()),
+    url(r'^register/$', views.RegisterFormView.as_view()), # Регистрация 
+    url(r'^login/$', views.LoginFormView.as_view()), # Аутентификация
     url(r'^logout/$', views.LogoutView.as_view()),
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^product/$', views.product, name='product'),
-    url(r'^product/?select=max', views.product, name='product'),
-    url(r'^product/?select=min', views.product, name='product'),
-    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^$', TemplateView.as_view(template_name='home.html')), 
+    url(r'^product/$', views.product, name='product'), # Товары
+    url(r'^product/?select=max', views.product, name='product'), # Сортировка товаров от макисмальной цены к минимальной
+    url(r'^product/?select=min', views.product, name='product'), # Сортировка товаров от минимальной цены к максимальной
+    url(r'^contact/$', views.contact, name='contact'), # Пользователи
 ]
