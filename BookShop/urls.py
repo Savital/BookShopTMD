6e466f7978658b26c24c565597c83a57b1ad792d
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-22 13:12:13 +0300

    Edited urlpatterns for api.

--- BookShop/urls.py
+++ BookShop/urls.py
@@ -16,7 +16,50 @@ from BookShop import views
 
 app_name = 'BookShop'
 urlpatterns = [
-    # @brief api interface for bookshop app
+    '''
+    This patterns of requests
+
+    Registration
+        Parameters: name, email, password
+        Method: POST
+    Example:
+        >>>url(r'^register/$', views.RegisterFormView.as_view())
+
+    Authenticaion
+        Parameters: name/email, password
+        Method: POST
+    Example:
+        >>>url(r'^login/$', views.LoginFormView.as_view())
+
+    Products
+        Parameters: ID, price, name
+        Method: GET
+    Example:
+        >>>url(r'^product/$', views.product, name='product')
+
+    Sort of Products by max
+        Parameters: max
+        Method: POST
+    Example:
+        >>>url(r'^product/?select=max', views.product, name='product')
+
+    Sort of Products by min
+        Parameters: min
+        Method: POST
+    Example:
+        >>>url(r'^product/?select=min', views.product, name='product')
+
+    Users
+        Parameters: name, email
+        Method: GET
+    Example:
+        >>>url(r'^contact/$', views.contact, name='contact')
+    '''
+    # Requests
+
+    # Registration
+    # Parameters: name, email, password
+    # Method: POST
 
 
     url(r'^register/$', views.RegisterFormView.as_view()),  # Registration
