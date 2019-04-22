376a926da6d84f251441089183db384db4ba2e97
Savostyanov Vitaliy <ssr.vitamin@gmail.com>
2019-04-22 13:33:57 +0300

    Edited urlpatterns for api.

--- BookShop/urls.py
+++ BookShop/urls.py
@@ -13,84 +13,68 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 from BookShop import views
 
+'''
+ This patterns of requests
+
+ Registration
+    Parameters: name, email, password
+    Method: POST
+ Example:
+    >>>url(r'^register/$', views.RegisterFormView.as_view())
+
+ Login
+     Parameters: name/email, password
+     Method: POST
+ Example:
+     >>>url(r'^login/$', views.LoginFormView.as_view())
+
+ Logout
+     Parameters: name/email, password
+     Method: POST
+ Example:
+     >>>url(r'^login/$', views.LoginFormView.as_view())
+
+ Home
+     Parameters:
+     Method: GET
+ Example:
+     >>>url(r'^login/$', views.LoginFormView.as_view())
+
+ Products
+    Parameters: ID, price, name
+    Method: GET
+ Example:
+    >>>url(r'^product/$', views.product, name='product')
+
+ Sort of Products by max
+    Parameters: max
+    Method: POST
+ Example:
+    >>>url(r'^product/?select=max', views.product, name='product')
+
+ Sort of Products by min
+    Parameters: min
+    Method: POST
+ Example:
+    >>>url(r'^product/?select=min', views.product, name='product')
+
+ Users
+    Parameters: name, email
+    Method: GET
+ Example:
+    >>>url(r'^contact/$', views.contact, name='contact')'''
+
 
 app_name = 'BookShop'
 urlpatterns = [
-    # This patterns of requests
-
-    # Registration
-    #    Parameters: name, email, password
-    #    Method: POST
-    # Example:
-    #    >>>url(r'^register/$', views.RegisterFormView.as_view())
-
-
     url(r'^register/$', views.RegisterFormView.as_view()),
-
-
-    # Login
-    #     Parameters: name/email, password
-    #     Method: POST
-    # Example:
-    #     >>>url(r'^login/$', views.LoginFormView.as_view())
-
-
     url(r'^login/$', views.LoginFormView.as_view()),
-
-    # Logout
-    #     Parameters: name/email, password
-    #     Method: POST
-    # Example:
-    #     >>>url(r'^login/$', views.LoginFormView.as_view())
-
-
     url(r'^logout/$', views.LogoutView.as_view()),
-
-    # Home
-    #     Parameters:
-    #     Method: GET
-    # Example:
-    #     >>>url(r'^login/$', views.LoginFormView.as_view())
-
-
     url(r'^$', TemplateView.as_view(template_name='home.html')),
-
-    # Products
-    #    Parameters: ID, price, name
-    #    Method: GET
-    # Example:
-    #    >>>url(r'^product/$', views.product, name='product')
-
-
     url(r'^product/$', views.product, name='product'),
-
-    # Sort of Products by max
-    #    Parameters: max
-    #    Method: POST
-    # Example:
-    #    >>>url(r'^product/?select=max', views.product, name='product')
-
-
     url(r'^product/?select=max', views.product,
         name='product'),
-
-    # Sort of Products by min
-    #    Parameters: min
-    #    Method: POST
-    # Example:
-    #    >>>url(r'^product/?select=min', views.product, name='product')
-
-
-
     url(r'^product/?select=min', views.product,
         name='product'),
-
-    # Users
-    #    Parameters: name, email
-    #    Method: GET
-    # Example:
-    #    >>>url(r'^contact/$', views.contact, name='contact')
-
-
     url(r'^contact/$', views.contact, name='contact'),
 ]
